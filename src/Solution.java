eba32a9bf6c2a280b205cce44f287cd80020e5e4
ernjvr <ernjvr@gmail.com>
2023-03-27 14:03:34 +1300

    test

--- src/Solution.java
+++ src/Solution.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  *input on line 1 - n number of steps eg. 4
  *input on line 2 - actual steps eg. uddu
  *output - print the number of valleys eg. 1
- * @author admin
+ * @author admin.
  */
 public class Solution {
 
